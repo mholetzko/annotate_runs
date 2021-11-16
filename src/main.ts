e70e21686e63bc2e19b8f99043f5b00a4f13e120
Matthias <mholetzko@gmx.net>
2021-11-16 08:51:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    core.error('This is a bad error. This will also fail the build.')
+    core.warning('Something went wrong, but it\'s not bad enough to fail the build.')
+    core.notice('Something happened that you might want to know about.')
     core.notice('Hello World ${ms} (... or run)')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
