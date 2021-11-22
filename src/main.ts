9329417740066030a86c6fb0aa670c8d51bee5e1
Matthias <mholetzko@gmx.net>
2021-11-22 11:18:55 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    console.log("... hello world")
     core.error('This is a bad error. This will also fail the build.')
     core.warning('Something went wrong, but it\'s not bad enough to fail the build.')
     core.notice('Something happened that you might want to know about.')
